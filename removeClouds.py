0454a9eab0491031b963dff2d1ebeae56bb55b5b
ssc40 <ssc.3.14159@gmail.com>
2025-06-29 15:22:57 -0700

    Update removeClouds.py

--- removeClouds.py
+++ removeClouds.py
@@ -9,7 +9,6 @@ import glob
 
 import glob
 from matplotlib import image as mpimg
-%matplotlib inline
 
 baseDir = 'rotations-png'
 rotDir = os.path.join(baseDir, 'train', '*')
@@ -22,9 +21,9 @@ imgs = glob.glob(rotDir)
 # HAVE TO FIX THIS -- color ranges are still set for red, need to change it for whiter colors.
 
 
-# lower boundary RED color range values; Hue (0 - 10)
-lower1 = np.array([0, 80, 20])
-upper1 = np.array([10, 255, 255])
+# fair weather rgb range
+lowerFairWeather = np.array([240, 240, 240])
+upperFairWeather = np.array([255, 255, 255])
  
 # upper boundary RED color range values; Hue (160 - 180)
 lower2 = np.array([160, 100, 20])
@@ -38,20 +37,18 @@ for n in range(len(imgs)):
     result = image.copy()
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
-    lower_mask = cv2.inRange(image, lower1, upper1)
-    upper_mask = cv2.inRange(image, lower2, upper2)
-    full_mask = lower_mask + upper_mask;
+    fairWeather_mask = cv2.inRange(image, lowerFairWeather, upperFairWeather)
 
-    result = cv2.bitwise_and(result, result, mask=full_mask)
-    dim = np.shape(full_mask)[0] 
-    counts = np.count_nonzero(full_mask)
+    result = cv2.bitwise_and(result, result, mask=fairWeather_mask)
+    dim = np.shape(fairWeather_mask)[0] 
+    counts = np.count_nonzero(fairWeather_mask)
 #     print(counts)
 #     print(dim**2)
     percent = 100*counts/dim**2
     subtitle_string = f'{percent}% of the image is red'
     filename = testImgPath.split('\\')[-1]
 #     print(f'{filename}, {subtitle_string}')
-    if percent > 50.0:
+    if percent > 10.0:
         counter += 1
 #         print('To be removed')
     else:
